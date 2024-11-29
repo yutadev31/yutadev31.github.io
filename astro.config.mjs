10b1126abdcdadbae897efa9d6c8f1423bf5be36
Yuta <jp.yuta0131@gmail.com>
2024-11-29 22:56:57 +0900

    add: github pagesにデプロイするためのコードを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://yutadev31.github.io",
+});
