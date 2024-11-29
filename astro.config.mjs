266266b716ea268ad06ef7d341fb6f62c7563a36
Yuta <jp.yuta0131@gmail.com>
2024-11-30 00:28:14 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yutadev31.github.io",
+  integrations: [mdx(), tailwind()],
 });
