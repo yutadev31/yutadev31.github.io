158d2eb0ea9e2c0a86445837ed0dca877db4501b
Yuta <yuta256dev@gmail.com>
2026-09-06 13:23:08 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yutadev31.github.io',
+  integrations: [sitemap()],
 });
